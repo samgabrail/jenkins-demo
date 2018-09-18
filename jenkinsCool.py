f79d5dacdfd2f924683835aacb48e72c3fbf3c7d
Sam Gabrail <samgabrail@SamMac.local>
2018-09-18 15:33:53 -0400

    Made changes to test promotion

--- jenkinsCool.py
+++ jenkinsCool.py
@@ -1,2 +1,2 @@
 for i in range(10000000):
-	print 'Jenkins is cool!, eh?! Repeat many times from prod repo. Let's do it!'
+	print 'Jenkins is cool!, eh?! Repeat many times from prod repo. Let's do it now!'
